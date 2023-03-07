ebc8f6d5eb063630e02ae5985099e23b5531fc9b
Alek Westover <alek.westover@gmail.com>
2023-03-07 11:42:12 -0500

    new post

---
+++ tex_to_augmd.py
@@ -0,0 +1,14 @@
+
+"""
+replace \begin \end with begin end
+
+replace \section with #
+
+replace `` with "
+
+replace image stuff with the correct image stuff
+
+rmk: potentially need a script for merging two image folders...
+"""
+
+
