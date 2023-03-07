4144863d771e1117d1cb16eb6e9ff75cedec40db
Alek Westover <alek.westover@gmail.com>
2023-03-07 11:51:39 -0500

    new post

--- compile-feeds.py
+++ compile-feeds.py
@@ -39,8 +39,27 @@ for dir in os.listdir():
       all_feeds.append((file.replace(".html", ""), this_entry))
 
   with open(join(dir, "index.html"), "w") as f:
+    blob = """
+    <head>
+    <link href="formatting/pandoc.css" rel="stylesheet">
+    <link href="formatting/envbox.css" rel="stylesheet">
+    <link href="formatting/bars.css" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js" integrity="sha256-H3cjtrm/ztDeuhCN9I4yh4iN2Ybx/y1RM7rMmAesA0k=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_CHTML-full" type="text/javascript"></script>
+    </head>
+    <body>
+    """
+    f.write(blob)
+    # TODO: this is just part of what we write, this needs to be injected into something else
+    # UPDATE: This is trash, please just actually do it right.
+    # we really should have a template, and then insert this into that.
+
     f.write(topics[dir])
-    # future: this is just part of what we write, this needs to be injected into something else
+    f.write("</body>")
 
 os.chdir("..")
 with open("topic-elts.html", "w") as f:
