6cb487307432658af01262459248424148b04874
Alek Westover <alek.westover@gmail.com>
2023-03-24 18:24:21 -0400

    new post

--- tex_to_augmd.py
+++ tex_to_augmd.py
@@ -5,10 +5,34 @@ replace \begin \end with begin end
 replace \section with #
 
 replace `` with "
-
-replace image stuff with the correct image stuff
-
-rmk: potentially need a script for merging two image folders...
 """
 
+import sys
+import os
+
+input_file = sys.argv[1]
+
+with open(input_file, 'r') as f:
+    content = f.read()
+
+rules = {
+    "\\begin": "begin",
+    "\\end": "end",
+    "\\section": "#",
+    "`": "\""
+}
+
+for rule in rules:
+    content = content.replace(rule, rules[rule])
+
+"""
+with this scheme we would need to allow the following things in augmd:
+
+begin {lemma}
+end {lemma}
+
+etc
+which I'm fine with. just wanted to lyk
+
+"""
 
